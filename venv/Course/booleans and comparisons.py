a4b0438a833909fe78510d255f707621331a595b
Renetus Fidel Masanja <33121586+renefiedel@users.noreply.github.com>
2021-03-29 23:38:25 +0200

    55% done

---
+++ venv/Course/booleans and comparisons.py
@@ -0,0 +1,12 @@
+
+a = [3, 7, 2]
+b = a
+print(a == b)
+print(a is b)
+print(id(a), id(b))
+print('o' not in 'John')
+
+print(int(True))
+print(bool('-3')) # empty objects and zero is false
+print(42 + False)
+
